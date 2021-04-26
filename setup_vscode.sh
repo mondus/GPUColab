8c3d095d71a94c2be02de790a47f7d7406feb086
mondus <p.richmond@sheffield.ac.uk>
2021-04-26 19:58:05 +0100

    Update setup_vscode.sh

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -1,3 +1,6 @@
-!curl -fsSL https://code-server.dev/install.sh | sh
-!pip install -qqq pyngrok
-!nohup code-server --port 9000 --auth none &
+#!/bin/bash
+# Script to install and start a visual studio code server
+
+curl -fsSL https://code-server.dev/install.sh | sh
+pip install -qqq pyngrok
+nohup code-server --port 9000 --auth none &
