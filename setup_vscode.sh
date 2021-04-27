80497b79b08613911024aad8539fc668d17ca844
mondus <p.richmond@sheffield.ac.uk>
2021-04-27 13:49:12 +0100

    Update setup_vscode.sh

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script to install and start a visual studio code server
+echo PASSWORD IN SCRIPT IS
 echo $PASSWORD
 curl -fsSL https://code-server.dev/install.sh | sh
 pip install -qqq pyngrok
